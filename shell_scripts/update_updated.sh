9975057078e9e4f6ba1b069386bcdbd975827e3b
KJR020 <johnjiro1114@gmail.com>
2024-03-02 22:34:26 +0900

    Update script to update timestamp in Markdown files

--- shell_scripts/update_updated.sh
+++ shell_scripts/update_updated.sh
@@ -1,2 +1,27 @@
-DATE=`date +%Y-%m-%d %H:%M:%S`
-find . -name '*.md' -exec sed -i "s/updated: .*/updated: $DATE/" {} \;
+#!/bin/bash
+
+# 現在の日付と時刻を取得
+DATE=$(date '+%Y-%m-%d %H:%M:%S')
+
+# 現在のディレクトリ内のすべての.mdファイルを対象に処理
+FILES=$(find . -type f -name '*timestamp.md')
+
+for FILE in $FILES; do
+  echo "Processing $FILE..."
+  
+  # YAMLヘッダーの抽出
+  awk '/^---$/{flag++} flag==1 {print} /^---$/{flag++}' $FILE > header.md
+  
+  # publishedが空なら更新
+  if ! grep -q "^published: [^ ]" header.md; then
+    sed -i "" "/^published:.*$/s//published: $DATE/" header.md
+  fi
+
+  # updatedは常に更新
+  sed -i "" "/^updated:.*$/s//updated: $DATE/" header.md
+  
+  # 更新したヘッダーを元のファイルに統合
+  sed '1,/---/!b;//{//!d;};r header.md' $FILE > updated_file.md && mv updated_file.md $FILE
+  
+  rm header.md  # 一時ファイルの削除
+done
